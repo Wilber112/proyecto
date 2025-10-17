bf8f325341c32141ecf1c6e0463c360d45c575c7
agfonse2 <valen2008dl@gmail.com>
2025-10-16 20:11:15 -0500

    a

--- python/app.py
+++ python/app.py
@@ -21,7 +21,7 @@ app.secret_key = 'colegiocarlosalban'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'bdpython' 
+app.config['MYSQL_DB'] = 'rgis360' 
 mysql = MySQL(app)
 
 #-------------------FUCIONES AUXILIARES-------------------
